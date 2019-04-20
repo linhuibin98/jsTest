1b1d314a4a004b3b65e038a822ee952ba0f033f9
lhb1032352370 <1032352370@qq.com>
2019-04-20 23:18:53 +0800

    扩展

--- test/12.maxSubArray.js
+++ test/12.maxSubArray.js
@@ -1,4 +1,5 @@
 // 求 数组中任意相连项相加的最大值
+// 最大子串和
 
 const arr = [10, -5, 12, 1, 6, -1, 2, -5];
 
@@ -7,6 +8,9 @@ const arr = [10, -5, 12, 1, 6, -1, 2, -5];
  * @param {Number []} arr 
  * return Number
  */
+
+ // 方法一：
+/*
 
 function maxSubArray (arr) {
   let max = 0,
@@ -22,6 +26,27 @@ function maxSubArray (arr) {
     }
   }
   return max;
+}
+
+console.log(maxSubArray(arr)); // 25
+
+*/
+
+// 方法二
+
+function maxSubArray (arr) {
+  let thisSum =0,
+      max = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    thisSum += arr[i];
+    if (thisSum > max) {
+      max = thisSum;
+    } else if (thisSum < 0) {
+      thisSum = 0;
+    }
+  }
+  return max; 
 }
 
 console.log(maxSubArray(arr)); // 25
