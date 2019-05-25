0c2249545dda3bf7b8d838b3140e14d7765b9ba3
lhb1032352370 <1032352370@qq.com>
2019-05-25 10:13:14 +0800

    递归组件

---
+++ notes/19成长笔记/architect/13.vue-component/递归组件/App.vue
@@ -0,0 +1,43 @@
+<template>
+<!-- 递归组件 iview -->
+  <div id='app'>
+    <Menu>
+      <MenuItem>菜单1</MenuItem>
+      <MenuItem>菜单2</MenuItem>
+      <MenuItem>菜单3</MenuItem>
+      <Submenu>
+        <template #title>
+          <MenuItem>菜单4</MenuItem>
+        </template>
+        <menu-item>4-1</menu-item>
+        <menu-item>4-2</menu-item>
+        <menu-item>4-3</menu-item>
+      </Submenu>
+    </Menu>
+  </div>
+</template>
+
+<script>
+import Menu from './Menu.vue';
+import MenuItem from './MenuItem.vue';
+import Submenu from './Submenu.vue';
+
+export default {
+  data() {
+    return {
+      msg: 'hello'
+    }
+  },
+  components: {
+    Menu,
+    MenuItem,
+    Submenu
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    color: red;
+  }
+</style>
---
+++ notes/19成长笔记/architect/13.vue-component/递归组件/Submenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="title" @click="change">
+      <slot name='title'></slot>
+    </div>
+    <div class="submenu-item" v-show="flag">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      flag: false
+    }
+  },
+  methods: {
+    change() {
+      this.flag = !this.flag;
+    }
+  }
+}
+</script>
